7aa06d2c146ed41c975c1d1a76b4d9fff17b9171
Selman <selmanebrahim567@gmail.com>
2025-09-11 02:26:55 +0300

    first commit128

--- index.js
+++ index.js
@@ -2,45 +2,45 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const patientRouter =require("./routers/patientRoutes");
-const adminRouter=require("./routers/adminRoutes");
-const authRouter=require("./routers/authRouter")
-
-const receptionRouter=require("./routers/receptionRoutes")
+const patientRouter = require("./routers/patientRoutes");
+const adminRouter = require("./routers/adminRoutes");
+const authRouter = require("./routers/authRouter");
+const receptionRouter = require("./routers/receptionRoutes");
 require('dotenv').config();
+
 const app = express();
-app.use(cookieParser());
+
+// Database connection
 const db = async () => {
     try {
         await mongoose.connect("mongodb+srv://natnael:CqMd83LicZQ6eV4Z@cluster1.e4nlf6k.mongodb.net/HospitalManagement?retryWrites=true&w=majority&appName=Cluster1");
-        console.log('database connected');
-    } catch(err) {
-        console.log("database failed to connect",err);
-       
+        console.log('Database connected');
+    } catch (err) {
+        console.log("Database failed to connect", err);
     }
-}
+};
 
 db();
 
-
+// CORS configuration
 app.use(cors({
-    origin: 'http://localhost:3000', 
-    credentials: true, 
+    origin: [
+        'http://localhost:3000', // For local development
+        'https://hospital-management-client-qgn9.vercel.app' // Allow Vercel URL
+    ],
+    credentials: true,
 }));
 
+app.use(cookieParser());
 app.use(express.json());
-app.use("/patient",patientRouter)
-app.use("/admin",adminRouter)
-app.use("/reception",receptionRouter)
-app.use("/auth",authRouter)
 
+// Route setups
+app.use("/patient", patientRouter);
+app.use("/admin", adminRouter);
+app.use("/reception", receptionRouter);
+app.use("/auth", authRouter);
 
-
-
-
-
-
-
+// Start server
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
