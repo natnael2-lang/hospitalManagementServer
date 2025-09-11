a7e3e906f6fd1e6303ead0f37c48cd2a1425ec3d
natnael2-lang <natnaelmessay71@gmail.com>
2025-09-11 22:07:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ db();
 
 // CORS configuration
 app.use(cors({
-    origin: 'https://hospital-management-client-nine.vercel.app',
+    origin: 'https://hospital-management-client-delta.vercel.app',
     credentials: true,
 }));
 
