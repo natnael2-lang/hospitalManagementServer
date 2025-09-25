3c643f1f682140ff6144163f44b6f0d9c0b88582
Selman <selmanebrahim567@gmail.com>
2025-09-25 23:45:59 +0300

    appointment

--- controllers/receptions/controllers.js
+++ controllers/receptions/controllers.js
@@ -1,8 +1,27 @@
-
+import Appointment from "../../models/appointment";
 const receptionOnLoad=(req,res)=>{
        const role=req.role;
-       if(!role==="reception"){return res.statu(302).json({redirect:"/login"})}
+       if(!role==="reception"){return res.status(302).json({redirect:"/login"})}
        return res.status(200).send("accepted")
 } 
 
-module.exports={receptionOnLoad}
+const patientAppointment=async(req,res)=>{
+       const post=req.body
+       if(!post){
+              console.log("appointment schedule not send")
+             return  res.status(404).json({message:"appointment form not received"})
+       }
+
+       try{
+              
+               const appointment=new Appointment(post)
+               await appointment.save();
+               return res.status(202).json({message:"appointment created successfuly"})
+       }
+       catch(err){
+              return res.status(500).json({message:"servor error"})
+       }
+        
+}
+
+module.exports={receptionOnLoad,patientAppointment}
--- routers/receptionRoutes.js
+++ routers/receptionRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {receptionOnLoad} = require("../controllers/receptions/controllers.js");
+const {receptionOnLoad,patientAppointment} = require("../controllers/receptions/controllers.js");
 const middlewares=require("../middlewares/authMiddleware")
 
 router.route('/')
     .get(receptionOnLoad);
+router.route("/appointments")
+      .post(patientAppointment)
 
 module.exports = router; 
